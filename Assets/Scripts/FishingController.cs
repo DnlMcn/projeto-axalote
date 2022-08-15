daac5307b11fe7c98101caa3b1b05dd4cc31cbcf
Danilo Mancini <haomakk.himself@gmail.com>
2022-08-15 20:51:24 -0300

    Add character rotation following camera movement

--- Assets/Scripts/FishingController.cs
+++ Assets/Scripts/FishingController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class FishingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform cameraTransform;
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Fishing action
+            Debug.Log("Pescando...");
+        }
+
+        Vector3 cameraForward = cameraTransform.forward * 10;
+        playerTransform.LookAt(cameraForward);
     }
 }
