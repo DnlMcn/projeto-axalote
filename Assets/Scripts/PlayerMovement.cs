360b8d633dac132d5701880da7134829342b75be
deogos <diogo.alonso@etec.sp.gov.br>
2022-09-21 19:14:30 -0300

    Adição movimento barco

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 12f;
+    public float rotationSpeed = 30f;
+
+    Vector3 velocity;
+
+    void Update()
+    {
+        if (Input.GetKey("w")) transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if (Input.GetKey("s")) transform.Translate(Vector3.right * -speed * Time.deltaTime);
+
+        if (Input.GetKey("a")) transform.Rotate(Vector3.up * -rotationSpeed * Time.deltaTime);
+        if (Input.GetKey("d")) transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
